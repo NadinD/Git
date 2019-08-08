1c4e9cf7b3d5945c0c3fa0620e166f04e3ddfdbb
deykovanv <deykovanv@usue.ru>
2019-08-08 08:20:52 +0500

    Создание функции draw и рисование флагштока

--- Flag.py
+++ Flag.py
@@ -8,6 +8,11 @@ size = width, height = 800, 600
 # screen — холст, на котором нужно рисовать:
 screen = pygame.display.set_mode(size)
 
+def draw():
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (152, 118, 84), (345, 260, 5, 100), 0)
+
+draw()
 # смена (отрисовка) кадра:
 pygame.display.flip()
 # ожидание закрытия окна:
