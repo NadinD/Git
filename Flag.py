f5be255c45bcc433f08633be6d7dd6c7f5102f30
deykovanv <deykovanv@usue.ru>
2019-08-08 08:13:09 +0500

    Создание игрового цикла

---
+++ Flag.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+# инициализация Pygame:
+pygame.init()
+# размеры окна:
+size = width, height = 800, 600
+# screen — холст, на котором нужно рисовать:
+screen = pygame.display.set_mode(size)
+
+# смена (отрисовка) кадра:
+pygame.display.flip()
+# ожидание закрытия окна:
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+# завершение работы:
+pygame.quit()
