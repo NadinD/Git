847e3b6f7f14ea099caeb01d70ca8b211c0af9ad
deykovanv <deykovanv@usue.ru>
2019-08-08 08:37:47 +0500

    рисуем синий сегмент флага

--- Flag.py
+++ Flag.py
@@ -11,6 +11,8 @@ screen = pygame.display.set_mode(size)
 def draw():
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (152, 118, 84), (345, 260, 5, 100), 0)
+    pygame.draw.rect(screen, (0, 0, 255), (350, 280, 100, 20), 0) # blue
+
 
 draw()
 # смена (отрисовка) кадра:
